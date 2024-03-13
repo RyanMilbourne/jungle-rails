4664b64219454cb53bb71f0de5403781eb76ab79
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-03-13 10:05:52 -0700

    Create admin/categories controller/view and link_to

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Catergory.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: "Category created!"
+    else
+      render :new
+    end
+
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
